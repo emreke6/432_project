861037e7891f87216129a4a58cd1bc0467c01dab
Emre Kocer <emreke6@gmail.com>
2022-04-29 23:59:21 +0300

    initial commit

---
+++ RemoteServer_project/RemoteServer_project/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteServer_project
+{
+    public partial class Form1 : Form
+    {
+        bool terminating = false;
+        bool listening = false;
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> socketList = new List<Socket>(); // list of client socket connected to server
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+        private void listen_button_Click(object sender, EventArgs e)
+        {
+            string portnum = port.Text;
+            int port_num;
+            
+
+            if (Int32.TryParse(portnum, out port_num))
+            {
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, port_num));
+                serverSocket.Listen(3);
+
+                listening = true;
+                listen_button.Enabled = false;
+                Thread acceptThread = new Thread(new ThreadStart(Accept));
+                acceptThread.Start();
+
+                logs.AppendText("Started listening. \n");
+            }
+            else
+            {
+                logs.AppendText("Check the port number. \n");
+            }
+        }
+
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+                    socketList.Add(newClient);
+                    logs.AppendText("A client is connected. \n");
+
+                    Thread receiveThread = new Thread(new ThreadStart(Receive));
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        logs.AppendText("Socket has stopped working. \n");
+                    }
+                }
+            }
+        }
+        private void Receive()
+        {
+            Socket s = socketList[socketList.Count - 1]; //client that is newly added
+            bool connected = true;
+
+            while (!terminating && connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    s.Receive(buffer);
+
+                    string message = Encoding.Default.GetString(buffer);
+                    message = message.Substring(0, message.IndexOf("\0"));
+                    logs.AppendText(message + "\n");
+
+                    string[] nums = message.Split(' ');
+                    int num1 = Int32.Parse(nums[0]);
+                    int num2 = Int32.Parse(nums[1]);
+
+                    int result = num1 + num2;
+                    string send_msg = Convert.ToString(result);
+
+                    buffer = null;
+                    buffer = Encoding.Default.GetBytes(send_msg);
+
+                    s.Send(buffer);
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs.AppendText("A client is disconnected. \n");
+                    }
+
+                    s.Close();
+                    socketList.Remove(s);
+                    connected = false;
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+    }
+}
